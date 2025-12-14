a9e6f02676ea99048626eec6fa6e16e34ed09e3b
Zack Davis <zack.davis88@hotmail.com>
2025-12-14 08:27:28 -0600

    adding reusable UserData openapi component, adding missing error response for update password

--- src/routes/auth/auth.authenticateAuthToken.openapi.ts
+++ src/routes/auth/auth.authenticateAuthToken.openapi.ts
@@ -13,31 +13,7 @@
  *           description: Successful message
  *           examples: ["user successfully authenticated"]
  *         user:
- *           type: object
- *           description: Authenticated user's details
- *           properties:
- *             username:
- *               type: string
- *               description: Unique, lowercase, username for the user
- *               examples: ["johndoe"]
- *             displayName:
- *               type: string
- *               description: Unique, case-sensitive display name for the user
- *               examples: ["JohnDoe"]
- *             createdOn:
- *               type: string
- *               format: date-time
- *               description: Timestamp of when the user was created
- *               examples: ["2023-10-05T14:48:00.000Z"]
- *             updatedOn:
- *               type: string
- *               format: date-time
- *               description: Timestamp of when the user was last updated
- *               examples: ["2023-11-05T15:00:00.000Z"]
- *           required:
- *             - username
- *             - displayName
- *             - createdOn
+ *           $ref: "#/components/schemas/UserData"
  *       required:
  *         - message
  *         - user
