65b5534d5b5a002fe4fd9a42eb854cd455362f0d
Zack Davis <zack.davis88@hotmail.com>
2025-12-30 13:57:15 -0600

    allowing non-api routes, updating server startup logic

--- src/server/utils/configureRoutes.ts
+++ src/server/utils/configureRoutes.ts
@@ -1,57 +1,41 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs';
 import { NotFoundError } from './errors';
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from 'src/openapi';
-import customThemeStyles from 'src/openapi/custom-theme/custom-theme-styles';
-import customThemeToggle from 'src/openapi/custom-theme/custom-theme-toggle';
 
-const configureRoutes = (app: Express) => {
+let DEFAULT_BASE_URL = '/api';
+if (typeof process.env.BASE_URL === 'string' && process.env.BASE_URL.startsWith('/')) {
+  DEFAULT_BASE_URL = process.env.BASE_URL;
+}
+
+const configureRoutes = async (app: Express) => {
   const rootPath = process.env.NODE_ENV === 'production' ? 'dist' : 'src';
   const fileExtension = process.env.NODE_ENV === 'production' ? 'js' : 'ts';
   const routeFiles = fs.globSync(`./${rootPath}/routes/**/index.${fileExtension}`);
-  const apiRouter = express.Router();
+  const router = express.Router();
 
   const routeFilePromises = routeFiles.map(async (file) => {
     const routeModule = await import(path.resolve(file));
     if (routeModule.default && typeof routeModule.default === 'function') {
-      routeModule.default(apiRouter);
-      app.use('/api', apiRouter);
+      routeModule.default(router);
+      const { baseUrl } = routeModule;
+      if (typeof baseUrl === 'string' && baseUrl.startsWith('/')) {
+        app.use(baseUrl, router);
+      } else {
+        app.use(DEFAULT_BASE_URL, router);
+      }
     }
   });
 
-  return Promise.all(routeFilePromises).then(() => {
-    const documentationRouter = express.Router();
+  await Promise.all(routeFilePromises);
 
-    // Serve the custom JS and (optionally) CSS for theme toggling before the swagger static serve
-    documentationRouter.get('/docs/custom-theme.js', (_req, res) => {
-      res.type('application/javascript').send(customThemeToggle);
-    });
-    documentationRouter.get('/docs/custom-theme.css', (_req, res) => {
-      res.type('text/css').send(customThemeStyles);
-    });
-
-    documentationRouter.use('/docs', swaggerUi.serve);
-    documentationRouter.route('/docs').get(
-      swaggerUi.setup(swaggerSpec, {
-        customJs: ['/docs/custom-theme.js'],
-        customCssUrl: '/docs/custom-theme.css',
-        swaggerOptions: {
-          persistAuthorization: true,
-        },
-        customSiteTitle: 'Open CMS Documentation',
-      }),
-    );
-    app.use(documentationRouter);
-
-    const catchAllRouter = express.Router();
-    catchAllRouter.route('/{*splat}').all((_req, res) => {
-      const routeNotFoundError = new NotFoundError('API route not found');
-      res.sendError(routeNotFoundError);
-    });
-    app.use(catchAllRouter);
+  // After configuring all known routes, setup a catch-all that returns a 404 for any unknown route.
+  const catchAllRouter = express.Router();
+  catchAllRouter.route('/{*splat}').all((_req: Request, res: Response) => {
+    const routeNotFounderror = new NotFoundError('API route not found');
+    return res.sendError(routeNotFounderror);
   });
+  app.use(catchAllRouter);
 };
 
 export default configureRoutes;
